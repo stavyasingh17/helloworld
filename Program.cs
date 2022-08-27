c9e7c23951c5cc747cbb4327b91b2f58e602c2f0
Anish Bhat G <Anish Bhat G@MSI>
2022-08-26 22:20:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Transactions;
+
+namespace helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string stg = "Hello, World (2)!";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("Enter username");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter user's age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Username is: " + userName + " and the age is: " + age);
+        }
+    }
+}
